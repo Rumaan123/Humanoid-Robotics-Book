752c11131ef1985b3b063d1a5479f32716d6d4fd
Rumaan Iqbal <161339664+Rumaan123@users.noreply.github.com>
2025-12-19 22:37:10 +0500

    remove node_modules and docusaurus cache from git tracking

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("D:\\Humanoid-Robotics-Book\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\Humanoid-Robotics-Book\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\Humanoid-Robotics-Book\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\Humanoid-Robotics-Book\\src\\css\\custom.css"),
-];
